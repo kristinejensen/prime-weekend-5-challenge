ed93b85c6f631f1579512eb36ffcd91587a09cb2
Kristine Jensen <kristine.b.jensen@gmail.com>
2017-03-12 11:37:50 -0500

    GET route working

--- public/scripts/controllers/employee.controller.js
+++ public/scripts/controllers/employee.controller.js
@@ -1,14 +1,28 @@
-myApp.controller('EmployeeController', function(){
+myApp.controller('EmployeeController', ['$http', function($http){
   console.log('employee controller was loaded');
 
   var self = this;
+  var employeeList = {list: []}
+
+getEmployees();
+
+  function getEmployees(){
+    console.log('add employee button clicked');
+    $http({
+      method: 'GET',
+      url: '/employees'
+    }).then(function(response) {
+      console.log('response from server: ', response)
+      employeeList.list = response.data;
+      self.employeeArray = employeeList.list;
+    });
+  }
 
   self.addEmployee = function(){
     console.log('add employee button clicked');
   }
 
-  self.deleteEmployee = function(){
-    console.log('delete employee button clicked');
-  }
-
-}); // end of myApp controller
+  // self.deleteEmployee = function(){
+  //   console.log('delete employee button clicked');
+  // }
+}]); // end of my app controller
--- routes/routes.js
+++ routes/routes.js
@@ -11,14 +11,26 @@ var config = {
 
 var pool = new pg.Pool(config);
 
-
-
-
-
-
-
-
-
-
+router.get('/', function(req, res) {
+  console.log('hit my get employees route');
+  pool.connect(function(err, client, done) {
+    if(err){
+      console.log(err);
+      res.sendStatus(500);
+    }else{
+      // SELECT * FROM task;
+      client.query('SELECT * FROM employees', function(err, result) {
+        done(); // close the connection db
+        if(err){
+          console.log(err);
+          res.sendStatus(500); // the world exploded
+        }else{
+          console.log(result.rows);
+          res.status(200).send(result.rows);
+        }
+      });
+    }
+  });
+});
 
 module.exports = router;
